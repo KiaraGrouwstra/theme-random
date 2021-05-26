e4c1b3f5c22b4c5a6973ac4af228677b48cdc6b3
Ethan Doh <16962469+neoswallow@users.noreply.github.com>
2021-05-26 00:54:38 -0400

    Update fish_prompt.fish
    make changes to reflect theme prompt file location

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,2 +1,2 @@
 # name: random
-source $OMF_PATH/themes/(random choice (ls $OMF_PATH/themes/))/fish_prompt.fish
+source $OMF_PATH/themes/(random choice (ls $OMF_PATH/themes/))/functions/fish_prompt.fish
