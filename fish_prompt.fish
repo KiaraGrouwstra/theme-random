c70e893d3628b55c0ae52eff00e2cf0cdcbfc7bb
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-04 13:24:51 +0900

    rewrite beacuse source command did not wirk

--- fish_prompt.fish
+++
@@ -1,2 +0,0 @@
-# name: random
-source $OMF_PATH/themes/(random choice (ls $OMF_PATH/themes/))/functions/fish_prompt.fish
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+
+# name: random
+
+
+function fish_prompt
+## themes in omf could not used by source command
+## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
+
+
+## workaround for priority of fish_prompt.fish . It cause omf theme command fail.
+
+	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
+	rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
+	end
+
+
+ ## pickup a theme_to_enable from omf official repo
+	while true
+	set theme_to_enable (random choice (omf.index.query --type=theme ))
+	if test "random" != $theme_to_enable
+		if test "random_omf_theme" != $theme_to_enable
+		break
+		end
+	end
+	end
+
+
+## enable new theme
+	omf.cli.theme $theme_to_enable
+
+
+## fix prompt disappeared after enabling new theme.  
+	omf.cli.reload
+end
