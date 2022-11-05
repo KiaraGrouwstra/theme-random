519a91a8a16c2dd16f6d92f4af72142ea39c83e3
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-05 17:16:38 +0900

    formatting and commenting

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,7 +6,7 @@ function fish_prompt
 ## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
 
 
-## workaround for priority of fish_prompt.fish . It cause omf theme command fail. 
+## workaround for priority of fish_prompt.fish . It causes omf theme command failing. 
 ## omf theme this script file will not call rm command to fix the issue.
 	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
 	rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
@@ -15,27 +15,28 @@ function fish_prompt
 
 ## pickup a new local theme
 	while true
-	set theme_to_enable $(random choice $(omf.packages.list --theme ))
+	set theme_to_enable (random choice (omf.packages.list --theme))
 	
-
-		if test "random" != $theme_to_enable
-			if test "random_omf_theme" != $theme_to_enable
+		if test "random" != "$theme_to_enable"
+			if test "random_omf_theme" != "$theme_to_enable"
 			break
 			end
 		end
         end
 
-        ## enable theme
-        omf.theme.set $theme_to_enable
+        ## enable the theme
+        omf.theme.set "$theme_to_enable"
 	
 	
 	## force omf to enable a new theme when fish source dotfiles (i.e. omf reload)
-	printf "random" > $OMF_CONFIG/theme
+	printf "random" > "$OMF_CONFIG"/theme
 	
 	
 	##  command makes a new prompt line that fix prompt line disappeared after enabling new theme.
 	printf " \r "
-	## omf reload command will cause dead loop. It can now refresh prompt line.
+	## When random theme is enabled, omf reload command will cause dead loop.
+	## prompt line appears without theme after enabling a new theme.
+	## The new theme will be enabled after a command returns.
 	## omf.cli.reload
 
 	
